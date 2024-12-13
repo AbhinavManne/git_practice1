9f6fa769cc7c531a8d06fa6be2d530f968d2872d
AbhinavManne <126573331+AbhinavManne@users.noreply.github.com>
2024-12-14 00:31:25 +0530

    Update addition_python.py

--- addition_python.py
+++ addition_python.py
@@ -1,3 +1,14 @@
 a=2
 b=3
 print(a+b)
+
+# Python program to print the multiplication table of 10
+
+# Define the number for which we want to print the table
+number = 10
+
+# Loop through 1 to 10
+for i in range(1, 11):
+    # Multiply the number by the current value of i and print the result
+    print(f"{number} x {i} = {number * i}")
+
